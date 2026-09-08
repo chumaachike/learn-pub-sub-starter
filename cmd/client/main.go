050a085e5028ba719cb1ca9f649ddefec4d7382d
Chuma Achike <edwardachike@gmail.com>
2026-09-08 18:06:57 +0100

    add repl for client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -14,22 +14,63 @@ import (
 )
 
 func main() {
+	fmt.Println("Starting Peril client...")
 	connString := "amqp://guest:guest@localhost:5672/"
 
 	conn, err := amqp.Dial(connString)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
 	}
 	defer conn.Close()
+	fmt.Println("Peril game client connected to RabbitMQ!")
+
 	name, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("could not get username: %v", err)
+	}
+
 	_, q, err := pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, routing.PauseKey+"."+name, routing.PauseKey, pubsub.QueueTransient)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("could not subscribe to pause: %v", err)
 	}
-	fmt.Println(q.Name)
+	fmt.Printf("Queue %v declared and bound!\n", q.Name)
+
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
 
 	<-ctx.Done()
-	fmt.Println("Starting Peril client...")
+
+	gamestate := gamelogic.NewGameState(name)
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+		switch words[0] {
+		case "move":
+			if _, err := gamestate.CommandMove(words); err != nil {
+				fmt.Println(err)
+				continue
+			}
+		case "spawn":
+			if err = gamestate.CommandSpawn(words); err != nil {
+				fmt.Println(err)
+				continue
+			}
+		case "status":
+			gamestate.CommandStatus()
+		case "help":
+			gamelogic.PrintClientHelp()
+		case "spam":
+			fmt.Println("Spamming not allowed yet")
+
+		case "quit":
+			gamelogic.PrintQuit()
+			return
+		default:
+			fmt.Println("unkown command")
+
+		}
+	}
+
 }
